9a12be2814c06aa7b0b7f6aa6229c31f1d29a073
«Olga» <rusalochka7913@gmail.com>
2022-09-08 10:54:49 +0300

    void PrintArray

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,3 +5,15 @@
 
 string[] array1 = new string[5] {"Cat", "30", "Hello", "World", "Dog"};
 string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
